cd678ea2ae010d85b13f8a421fbebb6b204e6ed8
KangWooJu <space1102@naver.com>
2025-01-22 00:15:06 +0900

    feat:default package 업로드

---
+++ src/test/java/org/kangwooju/skeletoncode_socket/SkeletonCodeSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kangwooju.skeletoncode_socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonCodeSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
